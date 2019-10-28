64134596efcafecdcca7c54d8eb658920184399f
Karen Vardumyan <karen_v@instigatemobile.com>
2019-10-28 17:27:42 +0400

    Added "prettier" code formatter config file.

--- src/query.js
+++ src/query.js
@@ -1,9 +1,4 @@
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLList,
-  GraphQLInt
-} = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLInt } = require('graphql');
 const { _itemsList, _filtersList } = require('./types.js');
 
 const allFiltersData = require('../program_data/filters.json');
@@ -12,54 +7,67 @@ const items = allProgramsData.items;
 
 //Define the Query
 const Query = new GraphQLObjectType({
-  name: 'Query',
-  fields: {
-    allItems: {
-      type: _itemsList,
-      args: {
-        workoutTypes: { type: new GraphQLList(GraphQLString) },
-        trainers: { type: new GraphQLList(GraphQLString) },
-        programIntensity : { type: new GraphQLList(GraphQLString) },
-        time : { type: new GraphQLList(new GraphQLList(GraphQLInt)) }
-      },
-      resolve: function(source, args) {
-        return (function(args, items) {
-          return items.filter(function(item) {
-            if(args.workoutTypes !== undefined) {
-              return item.workoutType.some(type => args.workoutTypes.includes(type.title));
-            } else {
-              return item;
-            }
-          }).filter(function(item) {
-            if(args.trainers !== undefined) {
-              return item.trainers.some(trainer => args.trainers.includes(trainer.title));
-            } else {
-              return item;
-            }
-          }).filter(function(item){
-            if(args.programIntensity !== undefined) {
-              return args.programIntensity.includes(item.programIntensity.title);
-            } else {
-              return item;
-            }
-          }).filter(function(item){
-            if(args.time !== undefined) {
-              return args.time.some(time => !(time[0] > +item.workoutDurationMaximum.id ||
-                                              time[1] < +item.workoutDurationMinimum.id));
-            } else {
-              return item;
-            }
-          })
-        })(args, items);
-      }
+    name: 'Query',
+    fields: {
+        allItems: {
+            type: _itemsList,
+            args: {
+                workoutTypes: { type: new GraphQLList(GraphQLString) },
+                trainers: { type: new GraphQLList(GraphQLString) },
+                programIntensity: { type: new GraphQLList(GraphQLString) },
+                time: { type: new GraphQLList(new GraphQLList(GraphQLInt)) },
+            },
+            resolve: function(source, args) {
+                return (function(args, items) {
+                    return items
+                        .filter(function(item) {
+                            if (args.workoutTypes !== undefined) {
+                                return item.workoutType.some(type =>
+                                    args.workoutTypes.includes(type.title)
+                                );
+                            } else {
+                                return item;
+                            }
+                        })
+                        .filter(function(item) {
+                            if (args.trainers !== undefined) {
+                                return item.trainers.some(trainer =>
+                                    args.trainers.includes(trainer.title)
+                                );
+                            } else {
+                                return item;
+                            }
+                        })
+                        .filter(function(item) {
+                            if (args.programIntensity !== undefined) {
+                                return args.programIntensity.includes(item.programIntensity.title);
+                            } else {
+                                return item;
+                            }
+                        })
+                        .filter(function(item) {
+                            if (args.time !== undefined) {
+                                return args.time.some(
+                                    time =>
+                                        !(
+                                            time[0] > +item.workoutDurationMaximum.id ||
+                                            time[1] < +item.workoutDurationMinimum.id
+                                        )
+                                );
+                            } else {
+                                return item;
+                            }
+                        });
+                })(args, items);
+            },
+        },
+        allFilters: {
+            type: _filtersList,
+            resolve() {
+                return allFiltersData;
+            },
+        },
     },
-    allFilters: {
-      type: _filtersList,
-      resolve() {
-        return allFiltersData;
-      }
-    }
-  }
 });
 
 exports.Query = Query;
--- src/server.js
+++ src/server.js
@@ -1,27 +1,29 @@
 //get all the libraries needed
+const configs = require('../env-configs.json');
 const express = require('express');
-const log = require('console-log-level')({level: 'trace'});
+const log = require('console-log-level')({ level: configs.app.logLevel });
 const graphqlHTTP = require('express-graphql');
 const { GraphQLSchema } = require('graphql');
 const { Query } = require('./query.js');
 const cors = require('cors');
-const configs = require('../env-configs.json');
 
 //setting up the port number and express app
 const app = express();
 
 // Define the Schema
 const schema = new GraphQLSchema({
-  query: Query
+    query: Query,
 });
 
 app.use(cors());
 
 //Setup the nodejs GraphQL server
-app.use('/', graphqlHTTP({
-    schema: schema,
-    graphiql: configs.app.graphiql
-  })
+app.use(
+    '/',
+    graphqlHTTP({
+        schema: schema,
+        graphiql: configs.app.graphiql,
+    })
 );
 
 app.listen(configs.app.port);
