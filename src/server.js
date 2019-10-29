94e1e66a6dfb0f4bd60d1a292fc6c34224aee6e5
Karen Vardumyan <karen_v@instigatemobile.com>
2019-10-29 13:58:51 +0400

    Updated config file

--- src/server.js
+++ src/server.js
@@ -1,30 +1,39 @@
-//get all the libraries needed
+var graphiql;
+var logLevel;
+if(process.env.NODE_ENV === 'production') {
+    graphiql = false;
+    logLevel = 'info';
+} else if(process.env.NODE_ENV === 'development') {
+    graphiql = true;
+    logLevel = 'warn';
+}
 const configs = require('../env-configs.json');
 const express = require('express');
-const log = require('console-log-level')({ level: configs.app.logLevel });
+const log = require('console-log-level')({ level: logLevel });
 const graphqlHTTP = require('express-graphql');
 const { GraphQLSchema } = require('graphql');
 const { Query } = require('./query.js');
 const cors = require('cors');
 
-//setting up the port number and express app
 const app = express();
 
-// Define the Schema
+// Define the GraphiQL Schema
 const schema = new GraphQLSchema({
     query: Query,
 });
 
 app.use(cors());
 
-//Setup the nodejs GraphQL server
+// Setup the nodejs GraphQL server
 app.use(
     '/',
     graphqlHTTP({
         schema: schema,
-        graphiql: configs.app.graphiql,
+        graphiql: graphiql,
     })
 );
 
-app.listen(configs.app.port);
-log.info(`Server Running at localhost:${configs.app.port}`);
+app.listen(configs.port);
+log.info(`Server Running at localhost:${configs.port}`);
+
+
