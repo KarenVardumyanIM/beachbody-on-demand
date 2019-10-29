5e9b4178500487bcfc5573623c5ff9d384006783
Karen Vardumyan <karen_v@instigatemobile.com>
2019-10-29 15:14:45 +0400

    Updated config file

--- src/configs.js
+++ src/configs.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports.graphiql = false;
-    module.exports.logLevel = 'info';
+    module.exports.logLevel = 'warn';
 } else if (process.env.NODE_ENV === 'development') {
     module.exports.graphiql = true;
-    module.exports.logLevel = 'warn';
+    module.exports.logLevel = 'debug';
 }
